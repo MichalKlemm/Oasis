dad729de6c6d40e9757987888895b4521e9c8c07
MichalKlemm <Klemmmichal@lgmail.com>
2024-07-17 20:11:45 +0200

    Doplnění třídy
    generátor Uuid + uložení do databáze
    Založení nového uživatele - POST
    Získání informace o všech uživatelích
    Úprava informací o uživateli - PUT
    Propojení databáze s textovým souborem s personid

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.*;
 import repository.UserRepository;
 import dataUser.DataUser;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -18,19 +22,108 @@ public class MainController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/users/{id}")
-    public ResponseEntity<DataUser> getTutorialById(@PathVariable("id") int id) {
-        DataUser dataUser = userRepository.findById(id);
+    @GetMapping("/users")
+    public ResponseEntity<List<DataUser>> getAllUsers() {
+        try {
+            List<DataUser> dataUser = new ArrayList<DataUser>();
 
-        if (dataUser != null) {
-            return new ResponseEntity<>(HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            dataUser = userRepository.findAll();
+
+            if (dataUser.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(dataUser, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello world!";
+    @GetMapping("/user/{id}")
+    public ResponseEntity<DataUser> getUserById(@PathVariable("id") int id) {
+        DataUser dataUser = userRepository.findById(id);
+
+        if (dataUser != null) {
+            return new ResponseEntity<>(dataUser, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
+
+    @PostMapping("/user")
+    public ResponseEntity<String> saveUser(@RequestBody DataUser dataUser) {
+
+        if (dataUser != null) {
+
+            try {
+
+                File dataFile = new File("C:\\Users\\Public\\IntelliJ\\Oasis\\dataPersonId.txt");
+                Scanner scanner = new Scanner(dataFile);
+
+                dataUser.setPersonId("");
+
+                while (scanner.hasNextLine()) {
+                    String record = scanner.nextLine();
+
+                    DataUser du = userRepository.findByPersonId(record);
+                    if (du == null) {
+                        dataUser.setPersonId(record);
+                        break;
+                    }
+
+                }
+
+                if (dataUser.getPersonId() == "") {
+
+                    return new ResponseEntity<>("Neni k dispozici další personID.", HttpStatus.NOT_FOUND);
+                }
+
+                UUID uuid = UUID.randomUUID();
+                dataUser.setUuid(uuid.toString());
+
+                userRepository.save(dataUser);
+                return new ResponseEntity<>("Uloženo.", HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Došlo k chybě při ukládání dat.", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new ResponseEntity<>("Data pro uložení nenalezena.", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/user")
+    public ResponseEntity<String> updateUser(@RequestBody DataUser dataUser) {
+
+        if (dataUser != null) {
+
+            try {
+
+                userRepository.update(dataUser);
+
+                return new ResponseEntity<>("Opraveno.", HttpStatus.OK);
+
+            } catch (Exception e) {
+                return new ResponseEntity<>("Došlo k chybě při opravě dat.", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        else {
+            return new ResponseEntity<>("Data pro opravu nenalezena.", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") int id) {
+
+        try {
+
+            userRepository.deleteById(id);
+
+            return new ResponseEntity<>("Smazáno.", HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>("Došlo k chybě při mazání záznamu.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
 }
